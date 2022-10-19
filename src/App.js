e21a5eebd5acf69e800bcd2918422f2fc752a8f1
Ulna <g.alperenkurt@gmail.com>
2022-10-19 20:52:10 +0300

    added localStorage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import "./App.css";
 import "bulma/css/bulma.min.css";
 import Header from "./components/header/Header";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AddTask from "./components/addTask/AddTask";
 import { Box, Block } from "react-bulma-components";
 import Tasks from "./components/tasks/Tasks";
 
 function App() {
     const [toggle, setToggle] = useState(true);
-    const [taskList, setTaskList] = useState([]);
+    const [taskList, setTaskList] = useState(
+        () => JSON.parse(localStorage.getItem("taskList")) || []
+    );
+
+    useEffect(() => {
+        localStorage.setItem("taskList", JSON.stringify(taskList));
+    }, [taskList]);
 
     return (
         <Box id="box" className="column is-two-fifths">
             <Header toggle={toggle} setToggle={setToggle} />
             {toggle && <AddTask taskList={taskList} setTaskList={setTaskList} />}
             <Block className="tasks">
-                {taskList.map((task) => {
-                    return <Tasks key={task.id} task={task} setTaskList={setTaskList} />;
-                })}
+                {Boolean(taskList.length) &&
+                    taskList.map((task) => {
+                        return <Tasks key={task.id} task={task} setTaskList={setTaskList} />;
+                    })}
             </Block>
         </Box>
     );
