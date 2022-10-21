8ff7eb8af849b7d7da76e3b921e21640154b6e05
Ulna <g.alperenkurt@gmail.com>
2022-10-21 11:57:45 +0300

    I don't remember what I changed

--- src/components/tasks/Tasks.jsx
+++ src/components/tasks/Tasks.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { Message, Button } from "react-bulma-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash, faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 import "./Tasks.css";
 const Tasks = ({ task, setTaskList }) => {
+    const MySwal = withReactContent(Swal);
     const taskStyle = {
         textDecoration: task.isDone ? "line-through" : "none",
     };
@@ -21,12 +26,23 @@ const Tasks = ({ task, setTaskList }) => {
         >
             <Message.Header className="taskHeader">
                 <span>{task.deadline}</span>{" "}
-                <Button
-                    remove
-                    onClick={() =>
-                        setTaskList((list) => list.filter((item) => item.id !== task.id))
-                    }
-                />
+                <div>
+                    <Button color={"danger"}>
+                        <span className="icon is-small">
+                            <FontAwesomeIcon icon={faPenToSquare} />
+                        </span>
+                    </Button>{" "}
+                    <Button
+                        color={"danger"}
+                        onClick={() =>
+                            setTaskList((list) => list.filter((item) => item.id !== task.id))
+                        }
+                    >
+                        <span className="icon is-small">
+                            <FontAwesomeIcon icon={faTrash} />
+                        </span>
+                    </Button>
+                </div>
             </Message.Header>
             <Message.Body className="taskBody">
                 <span>{task.task}</span>
